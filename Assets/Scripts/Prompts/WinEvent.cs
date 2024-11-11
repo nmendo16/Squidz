0ec44d54ca306dd140721876680509b2e668440e
nmendo16 <nmendo16@my.centennialcollege.ca>
2024-11-11 10:04:25 -0500

    Loading MenuScreen
    after winning, MenuScreen loads

--- Assets/Scripts/Prompts/WinEvent.cs
+++ Assets/Scripts/Prompts/WinEvent.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class WinEvent : MonoBehaviour
 {
@@ -45,6 +46,7 @@ public class WinEvent : MonoBehaviour
         }
 
         // Optionally, you can stop the game (pause) here if you like
-        Time.timeScale = 0; // Pauses the game
+        Time.timeScale = 5; // Pauses the game
+        SceneManager.LoadScene("MenuScreen");
     }
 }
