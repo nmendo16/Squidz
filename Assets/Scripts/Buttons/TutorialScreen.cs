d7ef6a7f6f07d3d190321d15188af8f1e60413d2
Angelica cuadrado <angel.kira65@gmail.com>
2024-11-27 12:35:36 -0500

    - TUTORIAL SCREEN Add assets, some player and keycaps animation for movement, missing information about controllers and organize them better

---
+++ Assets/Scripts/Buttons/TutorialScreen.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialScreen : MonoBehaviour
+{
+    public Button buttQuit;
+    public GameObject title;
+    //public Image backgroundOverlay;
+
+    void Start()
+    {
+        title.SetActive(true);
+        buttQuit.gameObject.SetActive(true);
+        buttQuit.onClick.AddListener(QuitGame);
+        //backgroundOverlay.enabled = false;
+    }
+
+    void QuitGame()
+    {
+        Application.Quit();
+    }
+}
